fd67474cf6d13b42688e88000e8ec5883ab70e18
sprudello <110892985+sprudello@users.noreply.github.com>
2022-11-01 18:13:17 +0100

    Program.cs and MakeList.cs added
    User story 4 is almost finished with a small base for user story 5

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Chameleon
+{
+
+    internal class Program
+    {
+        static void Main()
+        {
+            //Makes a new variable for the MakeList class
+            MakeList list = new();
+            //Gets the path of the file from User
+            list.ListPath();
+            //Checks if the file already exists
+            if (File.Exists(list.path))
+            {
+                //Load LoadingFile class
+            }
+            else
+            {
+                list.Write();
+                list.ListSaver();
+            }
+
+            
+        }
+    }
+}
