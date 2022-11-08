6ad1645a42f75dd77fa35d984c4386e2d128b7ba
Timog <110891995+timog06@users.noreply.github.com>
2022-11-08 09:49:14 +0100

    Add files via upload

---
+++ Code/TakeList.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Threading;
+
+namespace Chameleon
+{
+    class TakeList
+    {
+        //Makes List for the 2 Languages needed
+        public List<string> mainlanguage = new List<string>();
+        public List<string> secondLanguage = new List<string>();
+
+        public void LoadFile(string path)
+        {
+            //Saves everything in the file from (path) in text
+            string text = File.ReadAllText(path);
+            //
+            string[] lines = text.Split("\r\n");
+            int words = lines.Length;
+
+            //Splits the contents of the files with "," into main- and secondlanguage 
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] items = lines[i].Split(",");
+                mainlanguage.Add(items[0]);
+                secondLanguage.Add(items[1]);
+            }
+        }
+    }
+}
